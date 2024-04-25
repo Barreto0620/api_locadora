4c659f718798e63fe57b4907e722b8d6bfd38463
Gabriel Barreto <gabrielprozds@gmail.com>
2024-04-25 00:11:13 -0300

    Continua...

--- public/app.js
+++ public/app.js
@@ -3,6 +3,17 @@ fetch('http://localhost:8080/filmes')
     .then(data => {
         const filmesList = document.getElementById('filmes-list');
         data.forEach(filme => {
-            
-        })
+            const filmeDiv = document.createElement('div');
+            filmeDiv.innerHTML = `
+        <h2>${filme.nome}</h2>
+        <img src="${filme.foto}" alt="${filme.nome}">
+        <p><strong>Descrição:</strong> ${filme.descricao}</p>
+        <p><strong>Elenco:</strong> ${filme.elenco}</p>
+        <hr>
+    `
+            filmesList.appendChild(filmeDiv);
+
+        });
     })
+
+    .catch(error => console.error('Erro ao carregar os Filmes: '.error));
